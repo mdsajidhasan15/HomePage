c50c4e253d6f26792e6aafbbe00a160b113464db
riadahmed <riad.zakir@selise.ch>
2020-02-06 18:58:12 +0600

    Table created using array data

--- script/script.js
+++ script/script.js
@@ -1,14 +1,71 @@
-function myCreateFunction() {
-    var table = document.getElementById("myTable");
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    cell1.innerHTML = "Name";
-    cell2.innerHTML = "Id";
-    cell3.innerHTML = "Number";
+var container = document.getElementById("myTable");
+var newTbody = document.createElement('tbody');
+newTbody.setAttribute('id', 'table-body');
+
+var tableHeader = ["Name", "ID", "Number"];
+var tableData = [
+    {
+        Name: "Student 1",
+        ID: "1",
+        Number: 1
+    },
+    {
+        Name: "Student 2",
+        ID: "2",
+        Number: 2
+    },
+    {
+        Name: "Student 3",
+        ID: "3",
+        Number: 3
+    }
+];
+var counter = 4;
+function initilizeTableHeader() {
+    tableHeader.forEach(elem => {
+        var x = document.createElement('th');
+        x.classList.add('table-header');
+        var y = document.createTextNode(elem);
+        x.appendChild(y);
+        container.appendChild(x);
+    });
+
+    initilizeData();
 }
 
-function myDeleteFunction() {
-    document.getElementById("myTable").deleteRow(0);
+function initilizeData() {
+    tableData.forEach(elem => {
+        var newTR = document.createElement('tr');
+        var newName = document.createElement('td');
+        var newId = document.createElement('td');
+        var newNumber = document.createElement('td');
+        var x = document.createTextNode(elem.Name);
+        var y = document.createTextNode(elem.ID);
+        var z = document.createTextNode(elem.Number);
+
+        newName.appendChild(x);
+        newId.appendChild(y);
+        newNumber.appendChild(z);
+
+        newTR.appendChild(newName);
+        newTR.appendChild(newId);
+        newTR.appendChild(newNumber);
+
+        newTbody.appendChild(newTR);
+    });
+
+    container.appendChild(newTbody);
 }
+
+function myCreateFunction() {
+    newTbody.innerHTML = ``;
+    tableData.push({
+        Name: `Student ${counter}`,
+        ID: `${counter}`,
+        Number: counter
+    });
+    initilizeData();
+    counter++;
+}
+
+initilizeTableHeader();
